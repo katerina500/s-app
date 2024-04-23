0a230dabb2efb02225ef14f6eea10c4c12e37b4d
katerina500 <152635190+katerina500@users.noreply.github.com>
2024-04-23 11:47:02 +0400

    Update HeaderWidget.jsx
    /

--- src/Header/HeaderWidget/HeaderWidget.jsx
+++ src/Header/HeaderWidget/HeaderWidget.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const HeaderWidget = () => {
     const [weatherData, setWeatherData] = useState(null)
     useEffect(() => {
-        fetch("https://api.openweathermap.org/data/3.0/onecall?q={Moscow,Russia}&appid={58004826ae6122beb213818c59676754}"). then((data)=>{console.log(data, "DATA IS LOADED")})
+        fetch("/"). then((data)=>{console.log(data, "DATA IS LOADED")})
     }, [])
 
     return (
